48187addd8e5b8f52a3c107dc9c76d7d4606ddc0
Orevaoghene <orevaoghene.raphael@gmail.com>
2024-01-17 23:38:54 -0800

    simple errors

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for i in range(0, 10):
         if i == 8 and j == 9:
             print("89")
         elif i < j:
-            print("{}{}".format(i,j), end=", ")
+            print("{}{}".format(i, j), end=", ")
