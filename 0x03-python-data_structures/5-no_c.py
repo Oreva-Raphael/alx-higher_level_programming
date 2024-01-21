a46aedfcbe42d84b556621f011e16ff32aa88dea
Orevaoghene <orevaoghene.raphael@gmail.com>
2024-01-21 10:24:21 -0800

    this God is good

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
     l_string =  list(my_string)
-    for w in range(len(l_string) -1, -1, -1):
-        if l_string[w] == 'c' or l_string[w] == 'C':
-            l_string.pop(w)
-    my_string = ''.join(l_string)
+    new_list = [char for char in l_string if char != 'c' and char != 'C']
+    my_string = ''.join(new_list)
     return my_string
