93b547292a3b7d5601ef7ce8922d0696c8763cce
Orevaoghene <orevaoghene.raphael@gmail.com>
2024-01-22 12:03:57 -0800

    tuples fixes

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,16 @@
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = tuple_a + (0, 0)
-    tuple_b = tuple_b + (0 ,0)
+    result = ()
+    len_a = len(tuple_a)
+    len__b = len(tuple_b)
 
-    first_tuple = (tuple_a[0] + tuple_b[0])
-    second_tuple = (tuple_a[1] + tuple_b[1])
-    return(first_tuple, second_tuple)
+    max_length = max(len_a, len__b)
+    
+    for i in range(2):
+        if i < len_a and i < len__b:
+            result += (tuple_a[i] + tuple_b[i]),
+        elif i >= len_a:
+            result += (tuple_b[i]),
+        elif i >= len__b:
+            result += (tuple_a[i]),
+    
+    return result
