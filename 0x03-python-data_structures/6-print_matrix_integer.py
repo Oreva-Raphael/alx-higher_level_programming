d44f829ba9945961cb2cacccf471454d772de9bc
Orevaoghene <orevaoghene.raphael@gmail.com>
2024-01-22 11:44:59 -0800

    new matrix idea

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print()
+        return
+
     for row in matrix:
         for num in row:
             print("{:d}".format(num), end=" ")
