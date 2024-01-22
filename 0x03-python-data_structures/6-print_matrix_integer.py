a322c5e7d19e09f69234e4c8a6f192a0b3887d81
Orevaoghene <orevaoghene.raphael@gmail.com>
2024-01-22 11:48:05 -0800

    matrix touches

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print()
-        return
+def print_matrix_integer(my_matrix=[[]]):
+    num_rows = len(my_matrix)
+    num_cols = len(my_matrix[0])
 
-    for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
+    for row_idx in range(num_rows):
+        for col_idx in range(num_cols):
+            print("{:d}".format(my_matrix[row_idx][col_idx]), end="")
+            if col_idx == num_cols - 1:
+                break
+            else:
+                print(" ", end='')
         print()
