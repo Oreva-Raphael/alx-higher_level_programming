bfaa87b1a83494e62d7a02bfc20eab5b395add95
Orevaoghene <orevaoghene.raphael@gmail.com>
2024-01-23 15:29:29 -0800

    task 9 must work

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def len_(my_list):
+    output = 0
+    for q in my_list:
+        output += 1
+    return output
+
+def max_integer(my_list=[]):
+    if len_(my_list) == 0:
+        return None
+    else:
+        greatest = my_list[0]
+        for w in my_list:
+            if w > greatest:
+                greatest = w
+        return greatest
