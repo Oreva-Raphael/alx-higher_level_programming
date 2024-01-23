1fdaa2e3380497d234a875f0732eb7f429612236
Orevaoghene <orevaoghene.raphael@gmail.com>
2024-01-22 18:05:10 -0800

    another max value code

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+def len_(my_list):
+    output = 0
+    for q in my_list:
+        output += 1
+    return output
+
+def max_integer(my_list=[]):
+    if len_(my_list) == 0:
+        return None
+    else:
+        greatest = my_list[0]
+        for w in my_list:
+            if w > greatest:
+                greatest = w
+        return greatest
