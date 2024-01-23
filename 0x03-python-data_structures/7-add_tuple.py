1b65d7e26aac7fec4b031bbad22f1a0d1c4a1dd6
Orevaoghene <orevaoghene.raphael@gmail.com>
2024-01-22 19:44:45 -0800

    last

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    res = ()
+    result = ()
     len_a = len(tuple_a)
     len_b = len(tuple_b)
 
     max_tuple = (len_a) if (len(tuple_a) > len(tuple_b)) else (len_b)
     for x in range(2):
-        if (x < len_a) & (x < len_b):
-            res += (tuple_a[x] + tuple_b[x]),
+        if (x < len_a) and (x < len_b):
+            result += (tuple_a[x] + tuple_b[x]),
         elif x >= len_a:
-            res += (tuple_b[x]),
+            result += (tuple_b[x]),
         elif x >= len_b:
-            res += (tuple_a[x]),
-    return res
+            result += (tuple_a[x]),
+    return result
