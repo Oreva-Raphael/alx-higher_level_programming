c90a746d8fac8d41e8307f9c3aec4ec7a354246a
Orevaoghene <oreva.raphael@gmail.com>
2024-03-13 21:49:36 -0700

    fixing a but

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for w in range(1, 101):
-        if w % 3 == 0 and w % 5 != 0:
+        if w % 3 == 0:
             print("{}".format('Fizz'), end=" ")
-        elif w % 5 == 0 and w % 3 != 0:
+        elif w % 5 == 0:
             print("{}".format('Buzz'), end=" ")
         elif w % 3 == 0 and w % 5 == 0:
             print("{}".format("FizzBuzz"), end = " ")
