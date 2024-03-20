a7feda73e35f5fa5b127ec9839e20a6fec289ce3
Orevaoghene <oreva.raphael@gmail.com>
2024-03-19 19:32:11 -0700

    MAtrix

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,13 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+def no_c(my_string):
+def new_in_list(my_list, idx, element):
+def print_reversed_list_integer(my_list=[]):
+def replace_in_list(my_list, idx, element):
+def element_at(my_list, idx):
+def print_list_integer(my_list=[]):
+def print_matrix_integer(matrix=[[]]):
+
+
+#endif
