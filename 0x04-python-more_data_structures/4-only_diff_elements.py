aa70e61cd330fd65a8339f1c4459ca805b059d35
Orevaoghene <oreva.raphael@gmail.com>
2024-03-20 22:11:04 -0700

    coding

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    if set_1 ^ set_2:
-        return set_1 ^ set_2
-    else:
-        return None
+    return set_1.symmetric_difference(set_2)
