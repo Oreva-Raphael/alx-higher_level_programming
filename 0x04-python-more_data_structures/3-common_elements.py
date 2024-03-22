f44febc4d3cb5e0af62787a358cf48146a558f28
Orevaoghene <oreva.raphael@gmail.com>
2024-03-22 06:24:46 -0700

    using operations

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    newSet = set()
-    # return (set_1.intersection(set_2))
-    for w in set_2:
-        for p in set_1:
-            if w == p:
-                newSet.add(i)
-    return newSet
+    compare = set_1 & set_2
+    return compare
